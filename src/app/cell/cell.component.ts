d9351c173ed6809a1ade0f6d8c0defa808d09a5b
elSenjor <elsenjor@web.de>
2024-01-26 17:05:03 +0100

    added css border attributes for cells

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cell',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cell.component.html',
   styleUrl: './cell.component.css'
 })
 export class CellComponent {
+  @Input()
+  borderRight: boolean = false;
 
+  @Input()
+  borderBottom: boolean = false;
+
+  ngOnInit() {
+    console.log("hello");
+  }
 }
