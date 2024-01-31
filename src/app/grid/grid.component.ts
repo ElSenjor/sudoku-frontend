27fa4cafb5d4d49436faa128d9fdc23ccf00baee
Florian Funke <florianfunke1803@gmail.com>
2024-01-31 16:31:43 +0100

    Sudoku string to array function started

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -10,4 +10,13 @@ import { BoxComponent } from '../box/box.component';
 })
 export class GridComponent {
 
+  sudokuString: string = "006040097040730010017092030600070080105060903020010006050980160090056070860020300" 
+  //sudokuArray: int[] = 
+  function sudokuStringToArray(sudokuString) {
+    digit1: number = sudokuString[0]
+    console.log(digit1)
+    //return sudokuArray
+  }
+
 }
+
