e4bc95058f132a3672b6e310addaf790e59536bb
Florian Funke <florianfunke1803@gmail.com>
2024-02-02 15:44:53 +0100

    Tried to share data between grid and box component but failed

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CellComponent } from '../cell/cell.component';
+// import { GridComponent } from '../grid/grid.component';
 
 @Component({
   selector: 'app-box',
@@ -9,5 +10,20 @@ import { CellComponent } from '../cell/cell.component';
   styleUrl: './box.component.css'
 })
 export class BoxComponent {
-
+  constructor(){
+    console.log("Hello, I am the constructor of the BoxComponent class");
+ /*    @Input() testString: String = '';
+    @Input() testArray: Number[][] = [[], [], [], [], [], [], [], [], []];
+      console.log("Hello, I am the constructor of the BoxComponent class");
+    console.log("testString: " + testString);
+    console.log("testArray: " + testArray); */  
+  }
+  // constructor(){
+    //console.log("Hello, I am the constructor of the BoxComponent class")
+    //@Input() sudokuArrayBoxwise2: number[][];
+    //console.log(sudokuArrayBoxwise2)
+    // sudokuArrayBoxwise: number [][];
+    
+    // sudokuArrayBoxwise = this.GridComponent.sudokuArrayBoxwise;
+  // }
 }
